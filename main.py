0ba815eafdd3afe89237425a1f42fe97e9a91cd4
em <emile_thompson@live.co.uk>
2022-03-18 14:25:44 +0000

    final changes

--- main.py
+++ main.py
@@ -3,50 +3,34 @@ from flask import Flask, request, jsonify
 import json
 import management
 
-
 flask_object = Flask(__name__)  # telling py i want to create a new server
-
 
 @flask_object.route('/', methods=["GET"])  # when user asks for nothing (/), run this method
 def home_page():
     return """
     Welcome to Emile's Server
     Use the following API's to navigate
-    
-
-
+    After http://127.0.0.1:5000/
+    Remove Spartan from records: /spartan/remove?id=sparta_id
+    Add Spartan to records: /spartan_add
+    View specific Spartan record by ID: /spartan/'enter spartan id here'   
     """
 
 @flask_object.route('/spartan_add', methods=['POST'])  # allows user to add sparta data by passing json file
 def add_spartan_api():
-
     management.json_load(v_dict)
-
     adding = management.add_spartan(v_dict)
-
     management.json_save(v_dict)
 
     return adding
 
-    # trainee_obj = Spartan(sparta_id, first_name_v, last_name, birth_year, birth_month, birth_day, course, stream)
-    # trainee_obj.print_all()
-    #
-    # sparta_dict[sparta_id] = trainee_obj
-
-
-
 # http://127.0.0.1:5000//spartan/<spartan_id> get certain employee data, return error message if id doesnt exists in system, return as string
 @flask_object.route('/spartan/<spartan_id>', methods=["GET"])
 def sparta_id_getter(spartan_id):
-    # Check the database, read from a file, etc
-    # from v_dict check the spartan_id key, if exists, return that data to sender
 
-    management.json_load(v_dict)
+    id_get = management.spartan_id_get(spartan_id)
+    return id_get
 
-    #obj_data[id_to_check] should be an object that i can use the class functions on but its not
-    id_to_check = v_dict[spartan_id]
-
-    obj_data = id_to_check
 
 # #http://127.0.0.1:5000/spartan/remove?id=sparta_id   This API should allow the user to remove a spartan from the system by passing the sparta_id in the query_string
 @flask_object.route('/spartan/remove', methods=['POST'])
@@ -63,18 +47,22 @@ def remove(): # can this be done in the <> method
 
     return f"User would like to remove: {id_to_remove}"
 
-
-
-
 @flask_object.route('/spartan', methods=["GET"])
 def list_all():
+    # with open("data.json") as employee_json:
+    #     employee_dict_json = json.load(employee_json)
+    #     for keys in employee_dict_json:
+    #         print(employee_dict_json[keys])
 
-    return "User would like to list the Spartans as one JSON object"
+    with open("data.json") as employee_json:
+        employee_dict_json = json.load(employee_json)
+        for keys in employee_dict_json:
+            return(employee_dict_json[keys])
 
 
 if __name__ == "__main__":
+
     sparta_dict = {}
     v_dict = sparta_dict
-
 
     flask_object.run(debug=True)  # dont use debug true when pushing to server
--- management.py
+++ management.py
@@ -1,24 +1,13 @@
 import json
 from spartan import Spartan
-import main
-from flask import Flask, request, jsonify
+from flask import request
 
-
-
-
-# def sparta_dicts():
-#     global sparta_dict
-#     sparta_dict = {}
-#     return sparta_dict
-
-def sparta_request():
-    sparta_data = request.data
-    return sparta_data
+# def sparta_request():
+#     sparta_data = request.data
+#     return sparta_data
 
 def add_spartan(sparta_dict_v):
     global v_dict
-    # json_load(v_dict)
-
 
     sparta_data = request.json
     try:
@@ -62,9 +51,11 @@ def add_spartan(sparta_dict_v):
 
     # call the method that will create the employee record
 
-    trainee_obj = Spartan(sparta_id, first_name_v, last_name, birth_year, birth_month, birth_day, course, stream)
-    trainee_obj.print_all()
+    trainee_obj = Spartan(sparta_id, first_name_v, last_name,
+                          birth_year, birth_month, birth_day,
+                          course, stream)
 
+    # trainee_obj.print_all()
     sparta_dict_v[sparta_id] = trainee_obj
 
     # json_save(v_dict)
@@ -86,6 +77,8 @@ def read_sparta_id(spartan_id):
     except Exception as ex:
 
         return False
+
+def list_prep(): pass
 
 
 def read_name(name_str):
@@ -152,11 +145,16 @@ def read_day(day_str):
 
 def json_load(sparta_dict_v):
     global sparta_dict
-    with open("data.json", "r") as jsonload:
-        return_spartan = json.load(jsonload)
+
+    try:
+        with open("data.json", "r") as jsonload:
+            return_spartan = json.load(jsonload)
+
+    except Exception as ex:
+        return ("Records empty")
 
     for spartan_id_key in return_spartan:
-        return_spartan[spartan_id_key]
+
         n_emp_id = return_spartan[spartan_id_key]["spartan_id"]
         n_f_name = return_spartan[spartan_id_key]["first_name"]
         n_l_name = return_spartan[spartan_id_key]["last_name"]
@@ -166,22 +164,76 @@ def json_load(sparta_dict_v):
         n_course = return_spartan[spartan_id_key]["course"]
         n_stream = return_spartan[spartan_id_key]["stream"]
 
-        new = Spartan(n_emp_id, n_f_name,n_l_name, n_y_ob, n_m_ob, n_d_ob, n_course, n_stream )
+        new = Spartan(n_emp_id, n_f_name, n_l_name, n_y_ob, n_m_ob, n_d_ob, n_course, n_stream )
         sparta_dict_v[spartan_id_key] = new
 
 
 def json_save(sparta_dict_v):
     global sparta_dict
     # want to get the keys from the dictionary of objects, and add those object values to a dictionary of dictionary and add that to the json file
-    temp_dict_of_dict = {}  # creating an empty dictionary to store the employee dictionary as visual data/ not objects
+    temp_dict_of_dict = {}  # creating an empty dictionary to store the spartan dictionary as visual data/ not objects
 
     for sparta_id in sparta_dict_v:  # iterating through the keys in the sparta dictionary
         multiple_sparta_obj = sparta_dict_v[sparta_id]  # creating a variable which contains the sparta_objects in the dictionary/ at the sparta id location
-        sparta_dict_json = multiple_sparta_obj.__dict__  # creating a variable which opens the sparta_object_dict and visualises it as a dictionary
-        temp_dict_of_dict[sparta_id] = sparta_dict_json  # using the temp dict of dict to store the employee_obj_dict at the respective employee id key
 
-    with open("data.json","w") as sparta_json_dict_of_dict:  # opening the json file in write mode, overides the data on there already
-        json.dump(temp_dict_of_dict, sparta_json_dict_of_dict,indent=4)  # dumping the dictionary of dicts in the json file and format
+        sparta_dict_json = multiple_sparta_obj.__dict__  # creating a variable which opens the sparta_object_dict and visualises it as a dictionary
+
+        temp_dict_of_dict[sparta_id] = sparta_dict_json  # using the temp dict of dict to store the sparta employee dict of info at the respective employee id key
+    try:
+
+        with open("data.json", "w") as sparta_json_dict_of_dict:  # opening the json file in write mode, overides the data on there already
+            json.dump(temp_dict_of_dict, sparta_json_dict_of_dict,indent=4)  # dumping the dictionary of dicts in the json file and format
+
+    except Exception:
+        return "File not found"
+
+
+# def spartan_obj_list():
 #
-# if __name__ == "__main__":
-#     sparta_dict = {}
+#     temp_dict = {}
+#     with open("data.json", "r") as jsonload:
+#         return_spartan = json.load(jsonload)
+#
+#     for spartan_id_key in return_spartan:
+#         n_emp_id = return_spartan[spartan_id_key]["spartan_id"]
+#         n_f_name = return_spartan[spartan_id_key]["first_name"]
+#         n_l_name = return_spartan[spartan_id_key]["last_name"]
+#         n_y_ob = return_spartan[spartan_id_key]["birth_year"]
+#         n_m_ob = return_spartan[spartan_id_key]["birth_month"]
+#         n_d_ob = return_spartan[spartan_id_key]["birth_day"]
+#         n_course = return_spartan[spartan_id_key]["course"]
+#         n_stream = return_spartan[spartan_id_key]["stream"]
+#
+#         new = Spartan(n_emp_id, n_f_name, n_l_name, n_y_ob, n_m_ob, n_d_ob, n_course, n_stream)
+#         temp_dict[spartan_id_key] = new
+#
+#         return temp_dict[spartan_id_key].print_all()
+
+def spartan_id_get(sparta_id):
+
+    with open("data.json", "r") as jsonload:
+        return_spartan = json.load(jsonload)
+
+    temp_dict = {}
+    if sparta_id in return_spartan.keys():
+        n_emp_id = return_spartan[sparta_id]["spartan_id"]
+        n_f_name = return_spartan[sparta_id]["first_name"]
+        n_l_name = return_spartan[sparta_id]["last_name"]
+        n_y_ob = return_spartan[sparta_id]["birth_year"]
+        n_m_ob = return_spartan[sparta_id]["birth_month"]
+        n_d_ob = return_spartan[sparta_id]["birth_day"]
+        n_course = return_spartan[sparta_id]["course"]
+        n_stream = return_spartan[sparta_id]["stream"]
+
+        new = Spartan(n_emp_id, n_f_name, n_l_name, n_y_ob, n_m_ob, n_d_ob, n_course, n_stream)
+        temp_dict[sparta_id] = new
+        return new.print_all()
+    else:
+        return "This ID is not in our records"
+
+
+
+
+
+
+
